b4ab362212351f4ca5b61b378327a69f8328139e
nullx <mail@nullx.me>
2020-08-02 22:30:15 +0200

    run.sh updated

--- run.sh
+++ run.sh
@@ -4,10 +4,16 @@
 if [ ! `builtin type -p docker` ];
 then
     echo "Docker not found. Please install before proceed.";
+    if [ ! `builtin type -p git` ];
+    then
+        echo "Git not found."
+        echo "Please get source code at /home/jon/testProject-master (git: https://github.com/nullxx/testProject.git)"
+        echo "Project requirements: node, npm, docker"
+    fi
     exit 1;
 fi
 
 # docker build
-docker build -t nullx/testproject
+docker build -t nullx/testproject .
 # docker run
 docker run nullx/testproject
